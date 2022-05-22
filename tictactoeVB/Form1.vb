76f7a3dc51171db13a65a9d20dcaf28c834fbd58
CjGalicia <carljeremygalicia@gmail.com>
2022-05-22 14:16:58 +0800

    Added Function to Reset Button

--- tictactoeVB/Form1.vb
+++ tictactoeVB/Form1.vb
@@ -1,3 +1,19 @@
 ﻿Public Class Form1
+    Private Sub resetButton_Click(sender As Object, e As EventArgs) Handles resetButton.Click
+        Reset()
+    End Sub
 
+    Public Function Reset()
+        NorthWest.Text = ""
+        North.Text = ""
+        NorthEast.Text = ""
+        West.Text = ""
+        Center.Text = ""
+        East.Text = ""
+        SouthWest.Text = ""
+        South.Text = ""
+        SouthEast.Text = ""
+        Return True
+
+    End Function
 End Class
