6e7e02b57bd2e310fa9f9b90b201c569bffa15b3
CjGalicia <carljeremygalicia@gmail.com>
2022-05-22 15:12:10 +0800

    Added Function to the tictactoe buttons

--- tictactoeVB/Form1.vb
+++ tictactoeVB/Form1.vb
@@ -1,7 +1,6 @@
 ﻿Public Class Form1
     Dim currentPlayer As Char = "X"
 
-
     Private Sub resetButton_Click(sender As Object, e As EventArgs) Handles resetButton.Click
         Reset()
         ShowTurn()
@@ -26,13 +25,51 @@
         ElseIf (currentPlayer = "O") Then
             currentPlayer = "X"
         End If
-
         turnLabel.Text = "Current Player: " + currentPlayer
         Return currentPlayer
     End Function
 
+    Private Sub NorthWest_Click(sender As Object, e As EventArgs) Handles NorthWest.Click
+        PrintButtonText(NorthWest)
+    End Sub
 
+    Public Function PrintButtonText(ByVal btn As Button)
+        If btn.Text = "" Then
+            btn.Text = currentPlayer
+            ShowTurn()
+        End If
+        Return True
+    End Function
 
+    Private Sub North_Click(sender As Object, e As EventArgs) Handles North.Click
+        PrintButtonText(North)
+    End Sub
 
+    Private Sub NorthEast_Click(sender As Object, e As EventArgs) Handles NorthEast.Click
+        PrintButtonText(NorthEast)
+    End Sub
 
+    Private Sub West_Click(sender As Object, e As EventArgs) Handles West.Click
+        PrintButtonText(West)
+    End Sub
+
+    Private Sub Center_Click(sender As Object, e As EventArgs) Handles Center.Click
+        PrintButtonText(Center)
+    End Sub
+
+    Private Sub East_Click(sender As Object, e As EventArgs) Handles East.Click
+        PrintButtonText(East)
+    End Sub
+
+    Private Sub SouthWest_Click(sender As Object, e As EventArgs) Handles SouthWest.Click
+        PrintButtonText(SouthWest)
+    End Sub
+
+    Private Sub South_Click(sender As Object, e As EventArgs) Handles South.Click
+        PrintButtonText(South)
+    End Sub
+
+    Private Sub SouthEast_Click(sender As Object, e As EventArgs) Handles SouthEast.Click
+        PrintButtonText(SouthEast)
+    End Sub
 End Class
